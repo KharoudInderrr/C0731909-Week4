ff1f2e2ed18cef18b7611a7535d04b4c79df59c8
visualstudio <visualstudio@122-PC18>
2019-01-30 10:30:26 -0500

    kharoud

--- New project/Program.cs
+++ New project/Program.cs
@@ -8,16 +8,24 @@ namespace New_project
 {
     class Program
     {
-        class Village
+        
+            static void Main(string[] args)
+            {
 
-        {
+            }
+        }
+    class Village
+    {
+
+    
+
             // Node is an ADT
             // What kind of data do we need in a node?
-            public Village nextVillage;
-            public String VillageName;
+             public Village nextVillage;
+            public String Villagename;
             public bool isAstrildeHere = false;
             
 
         }
     }
-}
+
